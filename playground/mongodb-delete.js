589b332db155bb78ead94062226f9471fa3fe44b
Bruno Tacio <bruno018@gmail.com>
2018-12-31 11:47:16 -0200

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,62 @@
+const { MongoClient, ObjectID } = require('mongodb')
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }, (err, client) => {
+  if (err) {
+    return console.log('Unable to connect do mongoDb')
+  }
+  const db = client.db('TodoApp')
+
+  // DeleteMany
+  // db.collection('ToDos')
+  //   .deleteMany({ 
+  //     text: 'Eat lunch' 
+  //   })
+  //   .then((result) => {
+  //     console.log(result)
+  //   }, (err) => {
+  //     console.log('Unable to delete document', err)
+  //   })
+
+  // DeleteOne
+  // db.collection('ToDos')
+  //   .deleteOne({ 
+  //     text: 'Walk the dog' 
+  //   })
+  //   .then((result) => {
+  //     console.log(result)
+  //   }, (err) => {
+  //     console.log('Unable to delete document', err)
+  //   })
+
+  // FindOneAndDelete
+  // db.collection('ToDos')
+  //   .findOneAndDelete({
+  //     completed: false
+  //   })
+  //   .then((result) => {
+  //     console.log(result)
+  //   }, (err) => {
+  //     console.log('Error: ', err)
+  //   })
+
+  //DeleteMany - Should delete 5 docs
+  db.collection('Users')
+    .deleteMany({
+      $or:
+      [
+        {
+          name: 'Bruno T'
+        }, { 
+          _id: new ObjectID('5c2a0519c48e03028e2285fa') 
+        } 
+      ]
+    })
+    .then((result) => {
+      console.log(JSON.stringify(result, undefined, 2))
+    }, (err) => {
+      console.log('Unable to delete docs', err)
+    })
+
+
+  client.close()
+})
